3cc6c054bf44b07452174e77d35d19ca75367460
Aldana Delicia <noealdana2833@gmail.com>
2022-11-09 23:25:35 -0300

    primer-entrega-delicia

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import CartWidget from "./CartWidget";
+ 
+const navbar = ( {greeting}) => {
+    return (
+        <div>
+            < CartWidget/>
+        </div>
+    );
+};
+
+
+export default navbar;
